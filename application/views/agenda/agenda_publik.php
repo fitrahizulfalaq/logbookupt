9be65fd1cb1c77bedb3cb2732f9f027abedca2ad
fitrahizulfalaq <fitrahizulfalaq@gmail.com>
2021-04-19 15:03:17 +0700

    total peserta edited

--- application/views/agenda/agenda_publik.php
+++ application/views/agenda/agenda_publik.php
@@ -8,8 +8,8 @@
           <span class="info-box-icon bg-info"><i class="far fa-flag"></i></span>
           <div class="info-box-content">
             <span class="info-box-text">Total Peserta</span>
-            <span class="info-box-number"><?= $this->fungsi->countValue("tb_agenda","total_peserta");?> peserta dari <?= $this->fungsi->pilihan_advanced("tb_agenda","tgl <=",date("Y-m-d"))->num_rows();?> pelatihan</span>
-            <span class="info-box-text"><?= $this->fungsi->pilihan_advanced("tb_agenda","tgl >=",date("Y-m-d"))->num_rows();?> / <?= $this->fungsi->pilihan("tb_agenda")->num_rows();?> terlaksana</span>
+            <span class="info-box-number"><?= $this->fungsi->countValue("tb_agenda","total_peserta");?> peserta dari <?= $this->fungsi->pilihan_advanced("tb_agenda","tgl <=",date("Y-m-d"))->num_rows();?> agenda</span>
+            <span class="info-box-text"><?= $this->fungsi->pilihan_advanced("tb_agenda","tgl >",date("Y-m-d"))->num_rows();?> / <?= $this->fungsi->pilihan("tb_agenda")->num_rows();?> terlaksana</span>
 
           </div>
           <!-- /.info-box-content -->
