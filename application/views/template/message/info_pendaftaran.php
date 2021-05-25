1316377b195b11d48fbff9fe640a068f0099b1f8
fitrahizulfalaq <fitrahizulfalaq@gmail.com>
2021-05-25 14:43:38 +0700

    editinfo

--- application/views/template/message/info_pendaftaran.php
+++ application/views/template/message/info_pendaftaran.php
@@ -3,8 +3,8 @@
   <h5><i class="icon fas fa-info"></i> Hal yang perlu disiapkan</h5>
   <p>
     1. <b>Foto Diri</b> (Boleh Selfie/ Kelihatan Wajah/ Tidak berkelompok) <a href="https://go.uptkukm.id/contoh-foto" target="_blank"><i class="fas fa-file"></i></a><br>
-    2. <b>Foto KTP</b> (Melebar & Terlihat jelas)  <a href="https://go.uptkukm.id/contoh-ktp" target="_blank"><i class="fas fa-file"></i></a><br>
-    3. <b>SPT</b> oleh UMKM masing-masing (Format Dokumen .pdf, bukan gambar) <a href="https://go.uptkukm.id/contoh-spt" target="_blank"><i class="fas fa-file"></i></a><br>
+    2. <b>Foto KTP</b> (Melebar & Jelas)  <a href="https://go.uptkukm.id/contoh-ktp" target="_blank"><i class="fas fa-file"></i></a><br>
+    3. <b>SPT</b> dari UMKM (Format Dokumen .pdf, bukan gambar) <a href="https://go.uptkukm.id/contoh-spt" target="_blank"><i class="fas fa-file"></i></a><br>
     4. <b>Scan Tanda Tangan</b> <a href="https://go.uptkukm.id/contoh-ttd" target="_blank"><i class="fas fa-file"></i></a><br>Bila belum memiliki bisa membuat <a href="https://tools.uptkukm.id/ttd" target="_blank"><i>Disini</i></a>
   </p>    
 </div>
